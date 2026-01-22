40681be981a4f9289d17250af8f9a40fdd15a575
VietDucNg <vietducnguyen.fit@gmail.com>
2026-01-22 08:18:11 +0100

    fix: add duplicate content on multiple click on nav-btn

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import './style.css';
 
 import loadHome from './home';
 
+const navBtns = document.querySelectorAll('.nav-btn');
 const homeBtn = document.querySelector('.home-btn');
 
-homeBtn.addEventListener('click', loadHome);
+
+navBtns.forEach(btn => btn.addEventListener('click', (e)=> {
+    if (e.target.classList.contains('active')) return;
+    
+    e.target.classList.add('active');
+
+    if (e.target == homeBtn) loadHome();
+}))
