e51dbdf4cbfd83127cda611766b5be24f95569fd
VietDucNg <vietducnguyen.fit@gmail.com>
2026-03-01 16:17:15 +0100

    fix: load new page on navBtn click insead of append

--- src/index.js
+++ src/index.js
@@ -8,16 +8,23 @@ import loadMenu from './menu';
 import loadContact from './contact';
 
 const navBtns = document.querySelectorAll('.nav-btn');
-const homeBtn = document.querySelector('.home-btn');
-const menuBtn = document.querySelector('.menu-btn');
-const contactBtn =  document.querySelector('.contact-btn')
+const mainDiv = document.querySelector('.main-div');
 
+const pages = {
+    home: loadHome,
+    menu: loadMenu,
+    contact: loadContact
+};
 
 navBtns.forEach(btn => btn.addEventListener('click', (e)=> {
-    if (e.target.classList.contains('active')) return;
-    e.target.classList.add('active');
+    const clickedBtn = e.currentTarget;
+    const pageName = clickedBtn.dataset.page;
 
-    if (e.target == homeBtn) loadHome();
-    else if (e.target == menuBtn) loadMenu();
-    else if (e.target == contactBtn) loadContact();
+    if (clickedBtn.classList.contains('active')) return;
+    else {
+        navBtns.forEach(btn => btn.classList.remove('active'));
+        clickedBtn.classList.add('active');
+        mainDiv.innerHTML = '';
+        pages[pageName]();
+    }
 }))
