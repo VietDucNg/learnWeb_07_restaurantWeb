4482c7ef83217daa5b122ce7c7e3e071cf6810da
VietDucNg <vietducnguyen.fit@gmail.com>
2026-03-08 18:27:59 +0100

    feat: handel heading click

--- src/index.js
+++ src/index.js
@@ -11,7 +11,18 @@ import loadContact from './contact';
 
 const navBtns = document.querySelectorAll('.nav-btn');
 const mainDiv = document.querySelector('.main-div');
+const heading = document.querySelector('header h1');
+const homeBtn = document.querySelector('.home-btn');
 
+// handle heading click
+function handleHeadingClick() {
+    resetPage(homeBtn);
+    loadHome();
+}
+
+heading.addEventListener('click', handleHeadingClick);
+
+// handle nav click
 const pages = {
     home: loadHome,
     menu: loadMenu,
@@ -20,15 +31,19 @@ const pages = {
 
 loadHome();
 
+function resetPage(clickedBtn) {
+    navBtns.forEach(btn => btn.classList.remove('active'));
+    clickedBtn.classList.add('active');
+    mainDiv.innerHTML = '';
+}
+
 navBtns.forEach(btn => btn.addEventListener('click', (e)=> {
     const clickedBtn = e.currentTarget;
     const pageName = clickedBtn.dataset.page;
 
     if (clickedBtn.classList.contains('active')) return;
     else {
-        navBtns.forEach(btn => btn.classList.remove('active'));
-        clickedBtn.classList.add('active');
-        mainDiv.innerHTML = '';
+        resetPage(clickedBtn);
         pages[pageName]();
     }
 }))
